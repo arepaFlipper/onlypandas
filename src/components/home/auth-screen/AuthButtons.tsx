4f8d0de519cdff0f092c5ccc7430597ddae7684e
Cristian F. Tovar <cristian00tovar@gmail.com>
2025-03-08 17:37:03 -0500

    💌 style the AuthButtons

--- src/components/home/auth-screen/AuthButtons.tsx
+++ src/components/home/auth-screen/AuthButtons.tsx
@@ -1,7 +1,25 @@
-const AuthButtons = () => {
-  return (
-    <div className='flex gap-3 flex-1 md:flex-row flex-col'>AuthButtons</div>
-  )
-}
+"use client";
+import { Button } from "@/components/ui/button";
+// import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useState } from "react";
 
-export default AuthButtons
+const AuthButtons = () => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className='flex gap-3 flex-1 md:flex-row flex-col'>
+      {["Sign up", "Login"].map((text) => (
+        <div key={text} className='flex-1'>
+          <Button
+            className='w-full'
+            variant={text === "Sign up" ? "default" : "secondary"}
+            disabled={loading}
+          >
+            {text}
+          </Button>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default AuthButtons;

d7e6b1c56a93d14fca9aeb7becb2491f467bad51
Cristian F. Tovar <cristian00tovar@gmail.com>
2025-03-08 18:18:57 -0500

    🍐 use kinde to authenticate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import HomeScreen from "@/components/home/home-screen/HomeScreen";
 import AuthScreen from "@/components/home/auth-screen/AuthScreen";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
-  const user = false;
+const Home = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log(`🏝️%cpage.tsx:8 - user`, 'font-weight:bold; background:#2ad500;color:#fff;'); //DELETEME:
+  console.log(user); // DELETEME:
   return (
     <main>
       {(user) ? <HomeScreen /> : <AuthScreen />}
     </main>
   );
-}
+};
+
+export default Home;
--- src/components/home/auth-screen/AuthButtons.tsx
+++ src/components/home/auth-screen/AuthButtons.tsx
@@ -1,6 +1,7 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
-// import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { useState } from "react";
 
 const AuthButtons = () => {
@@ -9,17 +10,19 @@ const AuthButtons = () => {
   return (
     <div className='flex gap-3 flex-1 md:flex-row flex-col'>
       {["Sign up", "Login"].map((text) => (
-        <div key={text} className='flex-1'>
+        <RegisterLink key={text} className='flex-1'>
           <Button
             className='w-full'
             variant={text === "Sign up" ? "default" : "secondary"}
             disabled={loading}
+            onClick={() => setLoading(true)}
           >
             {text}
           </Button>
-        </div>
+        </RegisterLink>
       ))}
     </div>
   );
 };
+
 export default AuthButtons;

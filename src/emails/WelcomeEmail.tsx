26f8d7c3c1200e3fc8a7139294d7d1c6002c8d88
Cristian F. Tovar <cristian00tovar@gmail.com>
2025-03-23 09:54:43 -0500

    🚍 fix the email service settings

--- src/emails/WelcomeEmail.tsx
+++ src/emails/WelcomeEmail.tsx
@@ -18,7 +18,7 @@ const WelcomeEmail = ({
   return (
     <Html>
       <Head />
-      <Preview>Welcome to OnlyHorse!</Preview>
+      <Preview>Welcome to OnlyPandas!</Preview>
       <Body style={main}>
         <Container style={container}>
           <Section style={message}>
@@ -31,7 +31,7 @@ const WelcomeEmail = ({
               alt='welcome icon'
               style={{ margin: "auto", borderRadius: 10 }}
             />
-            <Heading style={{ ...global.heading }}>Welcome to OnlyHorse!</Heading>
+            <Heading style={{ ...global.heading }}>Welcome to OnlyPandas!</Heading>
             <Text style={global.text}>Hello {userName},</Text>
             <Text style={global.text}>
               We&apos;re really happy to see you on our platform. We hope you have a great experience and enjoy
@@ -59,13 +59,13 @@ const WelcomeEmail = ({
               Thanks for subscribing! We will see you in the platform. 🙂❤
             </Text>
             <Link href={BASE_URL} style={{ ...global.button, margin: "10px auto" }}>
-              Visit OnlyHorse
+              Visit OnlyPandas
             </Link>
           </Section>
           <Hr style={global.hr} />
           <Section style={global.defaultPadding}>
             <Text style={footer.text}>
-              © {new Date().getFullYear()} OnlyHorse, Inc. All Rights Reserved.
+              © {new Date().getFullYear()} OnlyPandas, Inc. All Rights Reserved.
             </Text>
           </Section>
         </Container>
